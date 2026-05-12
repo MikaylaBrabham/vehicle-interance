b60367046f40b38e8e7f58ddfa1601cefef57cb3
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-12 17:37:50 -0500

    added semi, car, and moped

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,6 +1,6 @@
 package com.pluralsight;
 
-public class Main {
+public class Main extends Vehicle {
 
     public static void main(String[] args) {
 
@@ -22,23 +22,42 @@ public class Main {
 
 
         //create instance for Car
-
+        Car easyDrive = new Car();
 
         //use setters
+        easyDrive.setColor("Red");
+        easyDrive.setNumberOfPassengers(4);
+        easyDrive.setCargoCapacity(15);
+        easyDrive.setFuelCapacity(12);
 
         //print values to verify working
-
+        System.out.println("The Car is" + easyDrive.getColor());
+        System.out.println("The Car can have " + easyDrive.getNumberOfPassengers() + " of passengers.");
+        System.out.println("The Car holds " + easyDrive.getCargoCapacity() + " of items.");
+        System.out.println("The Car needs " + easyDrive.getFuelCapacity() + "gallons to have a full tank.");
 
 
         //create instance for SemiTruck
 
+        SemiTruck largestDrive = new SemiTruck();
+
         //use setters
+        largestDrive.setColor("Purple");
+        largestDrive.setNumberOfPassengers(1);
+        largestDrive.setCargoCapacity(100);
+        largestDrive.setFuelCapacity(300);
 
         //print values to verify working
+        System.out.println("The SemiTruck is" + largestDrive.getColor());
+        System.out.println("The SemiTruck can have " + largestDrive.getNumberOfPassengers() + " of passengers.");
+        System.out.println("The SemiTruck holds " + largestDrive.getCargoCapacity() + " of items.");
+        System.out.println("The SemiTruck needs " + largestDrive.getFuelCapacity() + "gallons to have a full tank.");
+
 
 
 
         //create instance for Hovercraft
+
 
 
         //use setters
