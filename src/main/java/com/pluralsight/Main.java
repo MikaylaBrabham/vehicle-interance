5554b067aa0ef21774073a11bcae656ab4b37603
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-12 18:28:19 -0500

    used f print statements for everything and added my old ones to cuurent. this has cut down my code so much

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -8,12 +8,15 @@ public class Main {
         //create instance for Moped
         Moped slowDrive = new Moped("Pink", 1, 5, 2);
 
-
         //print values to verify working
-        System.out.println("The Moped is " + slowDrive.getColor());
-        System.out.println("The Moped can have " + slowDrive.getNumberOfPassengers() + " of passengers.");
-        System.out.println("The Moped holds " + slowDrive.getCargoCapacity() + " of items.");
-        System.out.println("The Moped needs " + slowDrive.getFuelCapacity() + " gallons to have a full tank.\n");
+        System.out.printf("The SemiTruck\n is %s\n holds %d passengers\n holds %d items\n and takes %d gallons to be considered full\n\n",
+                slowDrive.getColor(), slowDrive.getNumberOfPassengers(), slowDrive.getCargoCapacity(), slowDrive.getFuelCapacity() );
+
+        //this is the old way i went about it for all
+       // System.out.println("The Moped is " + slowDrive.getColor());
+      //  System.out.println("The Moped can have " + slowDrive.getNumberOfPassengers() + " of passengers.");
+        //System.out.println("The Moped holds " + slowDrive.getCargoCapacity() + " of items.");
+        //System.out.println("The Moped needs " + slowDrive.getFuelCapacity() + " gallons to have a full tank.\n");
 
 
 
