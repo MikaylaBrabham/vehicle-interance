f84e59ce05a91fd654c7545bbad7fef45a0892f6
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-12 20:23:17 -0500

    added my firstchoice

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,45 +6,51 @@ public class Main {
     public static void main(String[] args) {
 
         //create instance for Moped
-        Moped slowDrive = new Moped("Pink", 1, 5, 2);
+        Moped slowDrive = new Moped("Pink", 1, 5, 2, true,
+                false, false, true, 85, 70);
 
         //print values to verify working
         System.out.printf("The SemiTruck\n is %s\n holds %d passengers\n holds %d items\n and takes %d gallons to be considered full\n\n",
-                slowDrive.getColor(), slowDrive.getNumberOfPassengers(), slowDrive.getCargoCapacity(), slowDrive.getFuelCapacity() );
+                slowDrive.getColor(), slowDrive.getNumberOfPassengers(), slowDrive.getCargoCapacity(), slowDrive.getFuelCapacity());
 
         //this is the old way i went about it for all
-       // System.out.println("The Moped is " + slowDrive.getColor());
-      //  System.out.println("The Moped can have " + slowDrive.getNumberOfPassengers() + " of passengers.");
+        // System.out.println("The Moped is " + slowDrive.getColor());
+        //  System.out.println("The Moped can have " + slowDrive.getNumberOfPassengers() + " of passengers.");
         //System.out.println("The Moped holds " + slowDrive.getCargoCapacity() + " of items.");
         //System.out.println("The Moped needs " + slowDrive.getFuelCapacity() + " gallons to have a full tank.\n");
 
 
-
         //create instance for Car
-        Car easyDrive = new Car("Red", 4, 15, 12);
+        Car easyDrive = new Car("Red", 4, 15, 12, true,
+                false, true, true, 70, 60);
 
         //print values to verify working
         System.out.printf("The SemiTruck\n is %s\n holds %d passengers\n holds %d items\n and takes %d gallons to be considered full\n\n",
-                easyDrive.getColor(), easyDrive.getNumberOfPassengers(), easyDrive.getCargoCapacity(), easyDrive.getFuelCapacity() );
-
+                easyDrive.getColor(), easyDrive.getNumberOfPassengers(), easyDrive.getCargoCapacity(), easyDrive.getFuelCapacity());
 
 
         //create instance for SemiTruck
-        SemiTruck largestDrive = new SemiTruck("Purple", 1, 100, 300);
+        SemiTruck largestDrive = new SemiTruck("Purple", 1, 100, 300, true,
+                false, true, false, 20, 30);
 
         //print values to verify working
         System.out.printf("The SemiTruck\n is %s\n holds %d passengers\n holds %d items\n and takes %d gallons to be considered full\n\n",
-                largestDrive.getColor(), largestDrive.getNumberOfPassengers(), largestDrive.getCargoCapacity(), largestDrive.getFuelCapacity() );
-
+                largestDrive.getColor(), largestDrive.getNumberOfPassengers(), largestDrive.getCargoCapacity(), largestDrive.getFuelCapacity());
 
 
         //create instance for Hovercraft
-        Hovercraft amphiDrive = new Hovercraft("black", 3, 20, 20);
+        Hovercraft amphiDrive = new Hovercraft("black", 3, 20, 20, true,
+                true, true, false, 60, 50);
 
         //print values to verify working
-        System.out.printf("The Hovercraft\n is %s\n holds %d passengers\n holds %d items\n and takes %d gallons to be considered full\n",
-                amphiDrive.getColor(), amphiDrive.getNumberOfPassengers(), amphiDrive.getCargoCapacity(), amphiDrive.getFuelCapacity() );
+        System.out.printf("The Hovercraft\n is %s\n holds %d passengers\n holds %d items\n and takes %d gallons to be considered full\n\n",
+                amphiDrive.getColor(), amphiDrive.getNumberOfPassengers(), amphiDrive.getCargoCapacity(), amphiDrive.getFuelCapacity());
 
+//my first choice
+        System.out.printf("The getaway vehicle working? %b\n its electric? %b\n it's traceable? %b\n it's hotwireable? %b\n the stealth rating is %d\n the fuel efficiency score is %d\n ",
+                easyDrive.isWorking(), easyDrive.isElectric(), easyDrive.isTrackable(), easyDrive.isHotwireable(), easyDrive.getStealthRating(), easyDrive.getFuelEfficiency());
+        easyDrive.disableTracking();
     }
+
 
 }
